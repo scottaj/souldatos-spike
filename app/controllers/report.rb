d9c42c16d29a77153cbe2a3e596944c127cb87d1
alexander <alexander@vbox.fedora>
2012-09-14 04:41:29 -0400

    Got text statistics sample report working with rinruby.

--- app/controllers/report.rb
+++ app/controllers/report.rb
@@ -3,7 +3,7 @@ SouldatosSpike.controllers :report do
     redirect "/report/#{params[:report_type].parameterize.underscore}"
   end
 
-  get :index, with: :id do
+  get :index, with: :id, priority: :low do
     # Render report not found page
     error 404
   end
@@ -13,6 +13,19 @@ SouldatosSpike.controllers :report do
   end
 
   post :average_grade_on_assignment do
-    
+    scores = Assignment.where(name: params[:assignment]).map {|a| a.total_score}.delete_if {|n| n <= 0}
+    R.scores = scores
+    data = {}
+    ["mean", "median", "min", "max", "var", "sd"].each do |method|
+      R.eval "#{method}_result<-#{method}(scores)"
+      data[:"#{method}"] = R.pull("#{method}_result")
+    end
+    logger.info data.inspect
+    render '/report/report_table', locals: {
+      page_title: "Report Summary",
+      table_title: params[:assignment],
+      table_headers: data.map {|k, v| k.to_s.capitalize},
+      table: data
+    }
   end
 end
