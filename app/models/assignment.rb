1f796d1ddda59fd3d108ce6743fba060263fe9c9
alexander <alexander@vbox.fedora>
2012-09-14 19:08:46 -0400

    Added callback to students and assignments to generate already existing assignments and questions for new students.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,6 +7,21 @@ class Assignment < ActiveRecord::Base
   validates_presence_of :name, :assignment_type
   validates_inclusion_of :assignment_type, within: ASSIGNMENT_TYPES
   validates_uniqueness_of :name, scope: :student_id
+
+  def generate_questions()
+    template = Assignment.where(name: self.name, student_id: nil).first
+    Question.where(assignment_id: template.id).each do |q|
+      new_q = Question.new
+      new_q.text = q.text
+      new_q.value = q.value
+      new_q.assignment_id = self.id
+      new_q.save
+    end
+  end
+
+  after_create do
+    generate_questions
+  end
   
   def total_points()
     return self.questions.inject(0) {|sum, q| sum + q.value}
--- app/models/student.rb
+++ app/models/student.rb
@@ -5,6 +5,20 @@ class Student < ActiveRecord::Base
   validates_format_of :udm_id, with: /T0\d{7}/
   validates_uniqueness_of :udm_id
 
+  def generate_assignments()
+    Assignment.where(student_id: nil).each do |assn|
+      new_assn = Assignment.new
+      new_assn.name = assn.name
+      new_assn.assignment_type = assn.assignment_type
+      new_assn.student_id = self.id
+      new_assn.save
+    end
+  end
+
+  after_create do
+    generate_assignments
+  end
+  
   def name()
     return "#{self.first_name} #{self.last_name}"
   end
